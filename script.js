7591b09ca70d53d982c5eb4c1aa7332327d004ac
Султан <carameleclipsimus@gmail.com>
2023-05-31 20:47:23 +0600

    feat: done

--- script.js
+++ script.js
@@ -18,8 +18,37 @@ function frontDoorResponse(line) {
 }
 // console.log(frontDoorResponse(summer));
 
-function frontDoorPassword(word) {
-  return word[0] + word.slice(1).toLowerCase();
-}
+// function frontDoorPassword(word) {
+//   return word[0] + word.slice(1).toLowerCase();
+// }
 
-console.log(frontDoorPassword("SUMMER"));
+// console.log(frontDoorPassword("SUMMER"));
+
+const CODE_WORK = [
+  "Compilers intensily bestow",
+  "On commencing without ego",
+  "Different processes ajar",
+  "Exit with zero quick",
+];
+
+function backDoorResponse(str) {
+  let newArr = [];
+  for (let i = 0; i < str.length; i++) {
+    let temp = str[i].trim();
+    newArr.push(temp[temp.length - 1]);
+  }
+  return newArr.join("");
+}
+console.log(
+  backDoorResponse([
+    "Stands so high   ",
+    "Huge hooves too",
+    "Impatiently waits for",
+    "    Reins and harness    ",
+    "Eager to leave",
+  ])
+);
+
+function backDoorPassword(word) {
+  return `${word[0].toUpperCase() + word.slice(1)}, please`;
+}
