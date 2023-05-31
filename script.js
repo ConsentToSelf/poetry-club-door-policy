c23418d4d9901bae16beac242eca23884f7820a0
Султан <carameleclipsimus@gmail.com>
2023-05-31 19:38:34 +0600

    feat: 1ts done

---
+++ script.js
@@ -0,0 +1,20 @@
+const summer = [
+  "Sunshine warming my toes,",
+  "Underwater fun with my friends.",
+  "Making homemade ice cream on the porch,",
+  "Many long nights catching fireflies.",
+  "Early morning walks to the creek,",
+  "Reveling in the freedom of lazy days.",
+];
+// const expectedLetters = ["S", "U", "M", "M", "E", "R"];
+
+function frontDoorResponse(line) {
+  //   let newArr = [];
+  //   for (let i = 0; i < line.length; i++) {
+  //     newArr.push(line[i][0]);
+  //   }
+  //   return newArr;
+  return line[0][0];
+}
+console.log(frontDoorResponse(summer));
+
